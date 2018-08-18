71d1fafd4b1ec2a3bb719b0c6153753fcc821279
Rafael Campos Nunes <rafaelnunes@engineer.com>
2018-08-18 09:35:15 -0300

    Rewrite the script in a more modern manner
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- html/assets/js/navigation.js
+++ html/assets/js/navigation.js
@@ -1,25 +1,22 @@
 // Adds burger functionallity
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
 
   // Get all "navbar-burger" elements
-  var $navbar_burgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
+  let navbar_burgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0)
 
   // Check if there are any navbar burgers
-  if ($navbar_burgers.length > 0) {
-
+  if (navbar_burgers.length > 0) {
     // Add a click event on each of them
-    $navbar_burgers.forEach(function ($el) {
-      $el.addEventListener('click', function () {
-
+    navbar_burgers.forEach(el => {
+      el.addEventListener('click', () => {
         // Get the target from the "data-target" attribute
-        var target = $el.dataset.target;
-        var $target = document.getElementById(target)
+        const target = el.dataset.target;
+        const $target = document.getElementById(target)
 
         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
+        el.classList.toggle('is-active');
         $target.classList.toggle('is-active')
-
       })
     })
   }
